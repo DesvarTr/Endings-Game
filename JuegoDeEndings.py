95b2d6b31cd1823452ed9571ae5f837b3cb13859
unknown <gonzalo.montezuma@gmail.com>
2023-08-28 15:22:04 -0600

    actualizacion 2

--- JuegoDeEndings.py
+++ JuegoDeEndings.py
@@ -1,1 +1,34 @@
-print("En esta ocasión nos encontramos en un ambiente muy denso y oscuro")
+#Holaxd, Realizado por Gonzalo Montezuma
+
+def VALIDACION(Opcioncilla):
+
+    if Opcioncilla == "Z" or Opcioncilla == "X":
+
+        pass
+
+    else:
+        print("ASEGU´RATE DE INGRESAR UNA OPCIÓN VÁLIDA\n")
+        Opcioncilla = input("Intentalo de nuevo! (Z) / (X) ").upper()
+        VALIDACION(Opcioncilla)
+
+print("""\nAyer fue una noche lluviosa y afortunadamente tuviste una noche de sueño tranquila...
+es martes, no tienes mucho que hacer, después de todo estás de vacaciones y puedes usar tu tiempo a gusto.
+      
+Extrañamente... te encuentras en un pequeño dilema, ¿deberías ir al gimnasio o de visita a un amigo?
+      
+PARA SELECCIONAR, ESCRIBE LA OPCIÓN QUE DESEES Y PRESIONA ENTER""")
+
+Opcion1 = input("\nIr al gimnasio (Z) / Visitar a un amigo (X) ").upper()
+
+VALIDACION(Opcion1)
+print("")
+
+if Opcion1 == "Z":
+    print("De camino al gimnasio ")
+
+elif Opcion1 == "X":
+    print("""Sales en tu vehículo, ya que su casa no está muy cerca... el viaje será largo, posiblemente te tome 30 minutos
+y tienes dos rutas por las cuales ir, talvez una de ellas sea más corta.""")
+    
+    OpcionX1 = input("\nTomar la ruta convencional (Z) / Tomar la ruta desolada (X) ").upper()
+
